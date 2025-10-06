61129005776a5ae84bfafa76c90c01ea515b0951
bonyvah <bekbolsun@tutamail.com>
2025-10-06 16:10:31 +0600

    sdf

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/asmanga")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://asmanga_user:7nsUCGqWbzBgeEg7xFKGqODXAkErDM03@dpg-d3hp96nfte5s73d4mu9g-a/asmanga")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
