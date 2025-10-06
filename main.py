eb7cfc712fed44dac79d17c416ace4010bd69a0d
bonyvah <bekbolsun@tutamail.com>
2025-10-06 16:20:12 +0600

    sdf

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://asmanga-uch.vercel.app/"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
